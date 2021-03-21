b38a237387ff2ebc014e9fa4161f60b798fd197c
Peter Spangler <petertspangler@outlook.com>
2021-03-20 18:15:14 -0700

    Next step would be deployment, so time to work on ChoreHub!

--- SpangAP/pizzaproject/pizzaproject/urls.py
+++ SpangAP/pizzaproject/pizzaproject/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken import views
+import django.contrib.auth.urls
+from stores.views import UserCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stores.urls'))
+    path('api-token-auth/', views.obtain_auth_token),
+    path('', include('stores.urls')),
+    path('register/', UserCreateView.as_view(), name='create_user'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- SpangAP/pizzavspizza_app/App.js
+++ SpangAP/pizzavspizza_app/App.js
@@ -1,15 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ListView from "./src/screens/components/function_list_view";
 import DetailView from "./src/screens/components/detail_view";
 import AddPizzeria from "./src/screens/drawer/addPizzeria.js";
-import ScreenB from "./src/screens/drawer/screenB.js";
-import ScreenC from "./src/screens/drawer/screenC.js";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import RegForm from "./src/screens/drawer/regForm.js";
+import LoginForm from "./src/screens/drawer/loginForm.js";
 import TabOne from "./src/screens/tabs/tab1.js";
 import TabTwo from "./src/screens/tabs/tab2.js";
 
@@ -24,22 +23,22 @@ renderTabComponents = () => (
   </Tab.Navigator>
 );
 
-renderScreenComponents = () => {
+renderScreenComponents = () => (
   <Stack.Navigator>
     <Stack.Screen name="Home" component={ListView} />
     <Stack.Screen name="Detail" component={DetailView} />
     <Stack.Screen name="Tabs" children={this.renderTabComponents} />
-  </Stack.Navigator>;
-};
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen name="Home" children={this.renderScreenComponents} />
-        <Drawer.Screen name="Add Pizzeria" component={AddPizzeria} />
-        <Drawer.Screen name="Screen B" component={ScreenB} />
-        <Drawer.Screen name="Screen C" component={ScreenC} />
+        <Drawer.Screen name="Add Pizza" component={AddPizzeria} />
+        <Drawer.Screen name="Registration" component={RegForm} />
+        <Drawer.Screen name="Login" component={LoginForm} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
@@ -48,8 +47,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#577399",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  baseText: {
+    color: "navy",
+    fontSize: 30,
+    fontStyle: "italic",
+  },
+  newText: {
+    color: "red",
+  },
+  pizzaImage: {
+    width: 200,
+    height: 200,
   },
 });
